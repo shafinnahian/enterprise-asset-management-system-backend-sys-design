248450df231a603db9c098e992c8ab4a59f69a64
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2024-03-06 23:43:16 +0600

    Complete User model + views integrate route for Users

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('enterprise/', include('enterprise.urls'))
+    path('enterprise/', include('enterprise.urls')),
+    path('user/', include('user.urls'))
 ]
